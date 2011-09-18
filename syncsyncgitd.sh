2e2ac5ba8a898fd6cee3406433003b62adf91bd9
Keiichiro Ui <keiichiro.ui@gmail.com>
2011-09-19 01:52:39 +0900

    2011-09-19 01:52:39 /Users/kui/toybox/github/syncsyncgit/syncsyncgit.sh

--- syncsyncgitd.sh
+++ syncsyncgitd.sh
@@ -62,7 +62,6 @@ read_setting_file(){
         do
             TARGET_LIST_FILES=("${TARGET_LIST_FILES[@]}" "$foo")
         done
-        echo ${#TARGET_LIST_FILES}
     done
     
     for param in INTERVAL DEFAULT_REPOSITORY DEFAULT_BRANCH PID_FILE LOG_FILE\
