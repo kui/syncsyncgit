ff3277ebc195700a14605467ca1e6c45704ff16a
Keiichiro Ui <keiichiro.ui@gmail.com>
2011-09-19 01:49:00 +0900

    2011-09-19 01:49:00 /Users/kui/toybox/github/syncsyncgit/syncsyncgit.sh

--- syncsyncgitd.sh
+++ syncsyncgitd.sh
@@ -58,6 +58,7 @@ read_setting_file(){
     TARGET_LIST_FILES=()
     for file in "${target_list_files[@]}"
     do
+        echo "${TARGET_FILES[@]}"
         TARGET_LIST_FILES=("${TARGET_FILES[@]}" "$file")
         echo "${TARGET_FILES[@]}"
     done
