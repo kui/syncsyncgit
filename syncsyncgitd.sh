1cd2389addd0923ca4bb926abfd848f2548a011a
Keiichiro Ui <keiichiro.ui@gmail.com>
2011-09-19 01:09:56 +0900

    2011-09-19 01:09:56 /Users/kui/toybox/github/syncsyncgit/syncsyncgit.sh

--- syncsyncgitd.sh
+++ syncsyncgitd.sh
@@ -71,7 +71,7 @@ read_setting_file(){
     do
         eval $echo '$param:	$'"$param"
     done
-    echo "TARGET_FILES: ${TARGET_FILES[@]} "
+    echo "TARGET_FILES: ${TARGET_FILES[@]}"
 }
 
 get_file_repo_branch(){
