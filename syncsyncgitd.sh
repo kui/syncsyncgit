7bf7fef242b36ea760b7e2f717dd08abac0fa506
Keiichiro Ui <keiichiro.ui@gmail.com>
2011-09-19 02:36:45 +0900

    2011-09-19 02:36:45 /Users/kui/toybox/github/syncsyncgit/syncsyncgit.sh

--- syncsyncgitd.sh
+++ syncsyncgitd.sh
@@ -228,7 +228,7 @@ sync_all(){
     do
         cat "$list_file" | while read line
         do
-            if ! [ $file ]
+            if ! [ "$file" ]
             then
                 file="$line"
                 continue
